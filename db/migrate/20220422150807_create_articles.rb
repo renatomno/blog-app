e378be7b29f08c55626a87de98828167b0976bef
Renato Oliveira <xrenatomno@gmail.com>
2022-04-22 12:59:25 -0300

    add exmple

--- db/migrate/20220422150807_create_articles.rb
+++ db/migrate/20220422150807_create_articles.rb
@@ -4,7 +4,7 @@ class CreateArticles < ActiveRecord::Migration[7.0]
       t.string :title
       t.text :body
 
-      t.timestamps
+      t.timestamps #created_at and updated_at
     end
   end
 end
